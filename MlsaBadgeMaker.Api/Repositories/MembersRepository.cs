c3dd6a1b9801cdfa50c6e3b759c48c323d5487d6
Gino Messmer <gino@messmerized.de>
2021-07-28 23:17:19 +0200

    Add repository

---
+++ MlsaBadgeMaker.Api/Repositories/MembersRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using LiteDB;
+using MlsaBadgeMaker.Api.Data.InfluencerApi;
+
+namespace MlsaBadgeMaker.Api.Repositories
+{
+    public class MembersRepository
+    {
+        private readonly ILiteDatabase _database;
+
+        public MembersRepository(ILiteDatabase database)
+        {
+            _database = database;
+        }
+
+        public Task<MlsaMember> FindAsync(string emailAddress)
+        {
+            var member = Collection.FindOne(x => x.StudentPartnerEmail == emailAddress);
+            return Task.FromResult(member);
+        }
+
+        public Task AddAsync(MlsaMember member)
+        {
+            Collection.Insert(member);
+            return Task.CompletedTask;
+        }
+
+        private ILiteCollection<MlsaMember> Collection => _database.GetCollection<MlsaMember>();
+    }
+}
