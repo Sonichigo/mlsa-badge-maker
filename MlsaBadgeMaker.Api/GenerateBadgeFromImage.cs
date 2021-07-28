a3dc8923bc1d5c46b58cc4f88545ddedd62b5417
Gino Messmer <gino@messmerized.de>
2021-07-28 20:19:23 +0200

    Create GenerateBadgeFromImage.cs

---
+++ MlsaBadgeMaker.Api/GenerateBadgeFromImage.cs
@@ -0,0 +1,28 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using MlsaBadgeMaker.Api.Data.InfluencerApi;
+using MlsaBadgeMaker.Api.Services;
+
+namespace MlsaBadgeMaker.Api
+{
+    public static class GenerateBadgeFromImage
+    {
+        [FunctionName(nameof(GenerateBadgeFromProfile))]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "badge")] HttpRequest req,
+            ILogger log)
+        {
+            var imageFormFile = req.Form.Files.GetFile("image");
+            var pictureStream = imageFormFile.OpenReadStream();
+
+            IAvatarGenerator generator = new ImageSharpAvatarGenerator();
+            var outputStream = await generator.GenerateAsync(pictureStream, MlsaLevel.New);
+
+            return new FileStreamResult(outputStream, "image/png");
+        }
+    }
+}
