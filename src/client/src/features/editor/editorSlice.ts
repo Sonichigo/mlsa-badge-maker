83799a3b14ac4f796d469dc6ad44d3ccfd41856a
Gino Messmer <git@ginomessmer.me>
2021-08-08 22:04:07 +0200

    Preview final avatar

--- src/client/src/features/editor/editorSlice.ts
+++ src/client/src/features/editor/editorSlice.ts
@@ -6,6 +6,7 @@ import { generateImage } from "./editorAPI";
 export interface EditorState {
   fileBlobUrl?: string;
   croppedFileBlobUrl?: string;
+  finalAvatarBlobUrl?: string;
   status: 'idle' | 'busy' | 'failed';
   statusMessage: string;
 }
@@ -41,6 +42,10 @@ export const editorSlice = createSlice({
       ...state,
       croppedFileBlobUrl: action.payload,
     }),
+    setFinalAvatarBlobUrl: (state, action: PayloadAction<string>) => ({
+      ...state,
+      finalAvatarBlobUrl: action.payload,
+    }),
   },
   extraReducers: (builder) => {
     builder
@@ -49,25 +54,27 @@ export const editorSlice = createSlice({
         status: 'busy',
         statusMessage: 'Generating image...',
       }))
-      // .addCase(generateAsync.rejected, (state) => ({
-      //   ...state,
-      //   status: 'failed',
-      //   statusMessage: 'Failed to generate image',
-      // }))
-      .addCase(generateAsync.fulfilled, (state) => ({
+      .addCase(generateAsync.rejected, (state) => ({
+        ...state,
+        status: 'failed',
+        statusMessage: 'Failed to generate image',
+      }))
+      .addCase(generateAsync.fulfilled, (state, action) => ({
         ...state,
         status: 'idle',
         statusMessage: '',
+        finalAvatarBlobUrl: action.payload
       }));
   }
 });
 
 // Actions
-export const { setFileBlobUrl, setCroppedFileBlobUrl } = editorSlice.actions;
+export const { setFileBlobUrl, setCroppedFileBlobUrl, setFinalAvatarBlobUrl } = editorSlice.actions;
 
 // Selectors
 export const selectFileBlobUrl = (state: RootState) => state.editor.fileBlobUrl;
 export const selectCroppedFileBlobUrl = (state: RootState) => state.editor.croppedFileBlobUrl;
+export const selectFinalAvatarBlobUrl = (state: RootState) => state.editor.finalAvatarBlobUrl;
 
 // Exports
 export default editorSlice.reducer;
--- src/client/src/features/preview/Preview.tsx
+++ src/client/src/features/preview/Preview.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { useAppSelector } from '../../app/hooks';
+import { selectFinalAvatarBlobUrl } from '../editor/editorSlice';
 
 const Preview = () => {
-  
+  const avatarBlobUrl = useAppSelector(selectFinalAvatarBlobUrl);
 
   return (
     <div>
       <h1>Preview</h1>
+      {avatarBlobUrl && <img src={avatarBlobUrl} />}
     </div>
   )
 }
